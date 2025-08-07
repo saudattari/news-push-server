07c53c7c389fdc90622dd49de2810f7eb50b529a
Mohammad Saud <mohammadsaudattari@gmail.com>
2025-08-07 13:25:15 +0500

    change

--- api/notify-news.js
+++ api/notify-news.js
@@ -15,16 +15,16 @@ module.exports = async (req, res) => {
     };
 
     const headers = {
-      Authorization: "Basic os_v2_app_vibl3a5cwbcgxfkf4vxokqi3o2ptaut7gu5ek4nygkglunjy7ebbgfwqcpvih7po7t6w7vzzlggdnloqx3hiioqnp7qcq6cggaolkfa",
+      Authorization: "key os_v2_app_vibl3a5cwbcgxfkf4vxokqi3o2ptaut7gu5ek4nygkglunjy7ebbgfwqcpvih7po7t6w7vzzlggdnloqx3hiioqnp7qcq6cggaolkfa",
       "Content-Type": "application/json; charset=utf-8"
     };
 
     const response = await axios.post("https://onesignal.com/api/v1/notifications", payload, { headers });
 
-    console.log("✅ Notification sent:", response.data);
+    console.log("Notification sent:", response.data);
     res.status(200).send("Notification sent");
   } catch (error) {
-    console.error("❌ Error:", error.message);
+    console.error("Error:", error.message);
     res.status(500).send("Push failed");
   }
 };
